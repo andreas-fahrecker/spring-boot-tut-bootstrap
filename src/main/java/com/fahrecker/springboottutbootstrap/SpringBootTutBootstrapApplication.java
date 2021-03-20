84233b911a054264bd7d2ee177e7e5da65ec89c7
Andreas Fahrecker <AndreasFahrecker@gmail.com>
2021-03-20 20:21:32 +0100

    Initial Commit
    Signed-off-by: Andreas Fahrecker <AndreasFahrecker@gmail.com>

---
+++ src/main/java/com/fahrecker/springboottutbootstrap/SpringBootTutBootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.fahrecker.springboottutbootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutBootstrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTutBootstrapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fahrecker/springboottutbootstrap/SpringBootTutBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fahrecker.springboottutbootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutBootstrapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
